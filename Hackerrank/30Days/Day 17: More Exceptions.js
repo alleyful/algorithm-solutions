606961b556393d392258efcd8e78736b1c1443c7
alley <alley@snaps.com>
2019-04-16 12:57:03 +0900

    [HackerRank > 30Days] - Day 17

---
+++ Hackerrank/30Days/Day 17: More Exceptions.js
@@ -0,0 +1,11 @@
+
+class Calculator {
+
+	power(num, po) {
+		if (num < 0 || po < 0) {
+			throw 'n and p should be non-negative';
+		} else {
+			return po < 1 ? 1 : num * this.power(num, po - 1);
+		}
+	}
+}
